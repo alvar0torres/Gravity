785d25ab2b9821caefe97568988066da56557da3
Alvaro <995.alvaro@gmail.com>
2021-12-01 22:00:34 +0100

    initial commit

---
+++ gravity-calculations/src/components/Calculator.js
@@ -0,0 +1,10 @@
+import styles from "./Calculator.module.scss";
+
+
+const Calculator = () => {
+    return <div className={styles.glass}>
+
+    </div>
+};
+
+export default Calculator;
---
+++ gravity-calculations/src/components/Header.js
@@ -0,0 +1,9 @@
+import styles from "./Header.module.scss";
+
+const Header = () => {
+    return <header className={styles.header}>
+        <h1>Gravity Calculations</h1>
+    </header>
+};
+
+export default Header;
